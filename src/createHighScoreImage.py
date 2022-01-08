2e97ee4d8ec9bc0057483f7cb0d6a56d0b4119d7
Eric Faris <ericfaris@gmail.com>
2022-01-07 21:13:43 -0500

    added error message to image if tableName and authorName is not found.

--- src/createHighScoreImage.py
+++ src/createHighScoreImage.py
@@ -5,16 +5,22 @@ import sys
 import urllib.parse
 
 if len(sys.argv) > 1:
-    tableName = sys.argv[1]
-    authorName = sys.argv[2]
-    version = sys.argv[3]
-    path = sys.argv[4]
-    fileName = sys.argv[5]
+  tableName = sys.argv[1]
+  authorName = sys.argv[2]
+  version = sys.argv[3]
+  path = sys.argv[4]
+  fileName = sys.argv[5]
+else:
+  tableName = "Fog, The (Gottlieb 1979)"
+  authorName = "HiRez00"
+  version = ''
+  path = "c:\\temp"
+  fileName = tableName + "_highscore.png"
 
-apiBaseUri = "http://localhost/api/v1/"
-convertUri = urllib.parse.quote(apiBaseUri + "convert")
-# scoreUri = apiBaseUri + "scoresByTableAndAuthor?tableName=" + urllib.parse.quote(tableName) + "&authorName=" + urllib.parse.quote("authorName")
-scoreUri = apiBaseUri + "scoresByTableAndAuthor?tableName=" + urllib.parse.quote("Judge Dredd (Bally 1998)") + "&authorName=" + urllib.parse.quote("VPW")
+
+apiBaseUri = "http://vpcbot.golandry.net:6080/api/v1/"
+convertUri = apiBaseUri + "convert"
+scoreUri = apiBaseUri + "scoresByTableAndAuthor?tableName=" + urllib.parse.quote(tableName) + "&authorName=" + urllib.parse.quote(authorName)
 
 headers = {
   'Authorization': 'Bearer ODYwMzEwODgxNTc3NDY3OTA0.YN5Y8Q.0P5EwvlXHG6YOtNfkWKt_xOFTtc',
@@ -22,18 +28,23 @@ headers = {
 }
 
 tables = (requests.request("GET", scoreUri, headers=headers)).json()
+scoreList = ''
 
-for score in tables[0]['scores']:
-  if score.get('user'):
-    print(score['user']['username'] + "   " + str(score['score']))
+if len(scoreList) > 0:
+  for score in tables[0]['scores'][:5]:
+    if score.get('user'):
+      scoreList += score['user']['username'] + "\t\t" + str(score['score']) + '\n'
+else:
+  scoreList += "tableName and authorName not found.\n\n"
+  scoreList += "tableName: " + tableName + "\n"
+  scoreList += "authorName: " + authorName + "\n"
 
-# payload = json.dumps({
-#   "text": "Rank\tUsefor r\t\tScore\n1\tEric\t\t1,200,000\n2\tapophis\t\t945,000\n3\tDondi\t\t\t567,890",
-#   "filePath": "c:\\temp\\hiscores.png"
-# })
+payload = json.dumps({
+  "text": scoreList
+})
 
-# response = requests.request("POST", url, headers=headers, data=payload)
-# imageString = response.text.replace('data:image/png;base64,', '')
+res = requests.request("POST", convertUri, headers=headers, data=payload)
+imageString = res.text.replace('data:image/png;base64,', '')
 
-# with open("c:\\temp\\" + fileName, "wb") as fh:
-#     fh.write(base64.decodebytes(imageString.encode()))
+with open(path + "\\" + fileName, "wb") as fh:
+    fh.write(base64.decodebytes(imageString.encode()))
